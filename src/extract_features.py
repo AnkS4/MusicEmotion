c79df7b2525c6d717da534437afce85538dab342
AnkS4 <anks@tuta.io>
2025-01-07 23:14:24 +0100

    Add feature extraction script with annotations handling

---
+++ src/extract_features.py
@@ -0,0 +1,25 @@
+from preprocess import AudioFeatureExtractor, flatten_features
+import pandas as pd
+
+
+feature_data = []
+annotations = pd.read_csv("data/static_annotations_averaged.csv")  # Load the annotations
+
+for _, row in annotations.iterrows():
+    song_id = row["song_id"]
+    file_path = f"/data/audios/{song_id}.mp3"  # Path to the audio file
+
+    print(f"Extracting features for {song_id}..")
+    # Extract features for the song
+    feature_extractor = AudioFeatureExtractor(file_path)
+    features = feature_extractor.extract_features()
+    flat_features = flatten_features(features)
+
+    # Add the extracted features and labels to the dataset
+    flat_features["valence_mean"] = row["valence_mean"]
+    flat_features["arousal_mean"] = row["arousal_mean"]
+    feature_data.append(flat_features)
+
+# Create a DataFrame for training
+dataset = pd.DataFrame(feature_data)
+dataset.to_csv("data/features.csv")
