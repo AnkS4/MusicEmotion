9bb57b65279c8cd950fdae2c3c4a3dec50649dd2
AnkS4 <anks@tuta.io>
2025-01-07 23:47:48 +0100

    Specify dtype for 'song_id' to be read as a string

--- src/extract_features.py
+++ src/extract_features.py
@@ -3,13 +3,13 @@ import pandas as pd
 
 
 feature_data = []
-annotations = pd.read_csv("data/static_annotations_averaged.csv")  # Load the annotations
+annotations = pd.read_csv("data/static_annotations_averaged.csv", dtype={'song_id': str})  # Load the annotations
 
 for _, row in annotations.iterrows():
     song_id = row["song_id"]
     file_path = f"/data/audios/{song_id}.mp3"  # Path to the audio file
 
-    print(f"Extracting features for {song_id}..")
+    print(f"Extracting features for {song_id}.mp3 ...")
     # Extract features for the song
     feature_extractor = AudioFeatureExtractor(file_path)
     features = feature_extractor.extract_features()
